a94933fe92616f83602baf5801f7564206bba16a
jawadhassan <jawadsom@yahoo.com>
2021-12-05 03:51:25 +0500

    Datatable working with post

---
+++ myblog/src/main/resources/static/js/users.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+
+/*$(document).ready(function() {
+    $('#example').DataTable();
+
+
+} );*/
+
+
+$(document).ready(function() {
+	
+		
+	const obj = {id:1, fullName: "John", email: "abc", enabled:false};
+	
+	var JSONVALUE = JSON.stringify(obj); 
+	
+	var token = $("meta[name='_csrf']").attr("content");
+	
+    $('#example').DataTable( {
+         processing: true,
+         serverSide : true,
+         ajax : {
+			 "headers": {"X-CSRF-TOKEN": token},	
+			 "dataType" : 'json',
+			 "contentType": "application/json", 
+			 "type" : 'POST',
+			 "url" : '/admin/check/1',
+			 "data": function(){
+                	 return JSON.stringify(obj);
+            	} 	
+			}
+    } );
+} );
